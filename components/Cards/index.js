bf73d07ff1316ca7ce99207133670d08e58ff505
Westley Feller <feller.westley@gmail.com>
2019-09-15 19:14:19 -0700

    Cards component completed

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,48 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+(async () => {
+  const cards = [];
+
+  try {
+    const response = await axios.get('https://lambda-times-backend.herokuapp.com/articles');
+    const { articles } = response.data;
+
+    Object.keys(articles).forEach((topic) => {
+      articles[topic].forEach((article) => {
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        const headline = document.createElement('div');
+        headline.className = 'headline';
+        headline.textContent = article.headline;
+        card.appendChild(headline);
+
+        const author = document.createElement('div');
+        author.className = 'author';
+        card.appendChild(author);
+
+        const cardImageContainer = document.createElement('div');
+        cardImageContainer.className = 'img-container';
+        author.appendChild(cardImageContainer);
+
+        const cardImage = document.createElement('img');
+        cardImage.src = article.authorPhoto;
+        cardImageContainer.appendChild(cardImage);
+
+        const authorName = document.createElement('span');
+        authorName.textContent = `By ${article.authorName}`;
+        author.appendChild(authorName);
+
+        cards.push(card);
+      });
+    });
+  } catch (e) {
+    console.log(e);
+  } finally {
+    cards.forEach((card) => {
+      document.querySelector('.cards-container').appendChild(card);
+    });
+  }
+})();
